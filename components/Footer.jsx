cb1a21943b6e5328d1d1704d6d113e27370bba5a
anonymous298 <tackletalha@gmail.com>
2025-08-27 01:01:21 +0500

    footer component created

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+"use client"
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-[#004030] p-1 w-[100%] flex justify-center items-center mt-5'>
+        <footer className=' text-center'>
+            <h1 className="logo text-white font-bold text-[18px] cursor-pointer"><span className='text-[#67C090]'>&lt;</span>Pass<span className='text-[#67C090]'>Fortress</span><span className='text-[#67C090]'>/&gt;</span></h1>
+            <p className='text-white text-[14px] font-semibold space-x-5'>Created With ❤️ By Talha</p>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
