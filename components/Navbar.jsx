3a6a75f267e74d99d98b16d526970f051a69a4f5
anonymous298 <tackletalha@gmail.com>
2025-08-27 11:23:24 +0500

    animation added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,16 +1,24 @@
 "use client"
 
 import React from 'react'
+import * as motion from "motion/react-client"
 
 const Navbar = () => {
   return (
-    <div className='bg-[#004030]'>
-      <nav className='flex w-[80%] items-center justify-between mx-auto p-1.5 max-[500px]:w-[88%] max-[400px]:w-[96%]'>
-        <h1 className="logo text-white font-bold text-[22px] cursor-pointer"><span className='text-[#67C090]'>&lt;</span>Pass<span className='text-[#67C090]'>Fortress</span><span className='text-[#67C090]'>/&gt;</span></h1>
+    <motion.div
+      initial={{ opacity: 0, y: 10 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.2, ease: "easeOut" }}
+    >
 
-        <div className=' border-2 border-[#67C090] px-5 py-1 rounded-[20px] font-bold text-white flex items-center justify-center hover:bg-green-500 cursor-pointer transition-all'>Login</div>
-      </nav>
-    </div>
+      <div className='bg-[#004030]'>
+        <nav className='flex w-[80%] items-center justify-between mx-auto p-1.5 max-[500px]:w-[88%] max-[400px]:w-[96%]'>
+          <h1 className="logo text-white font-bold text-[22px] cursor-pointer"><span className='text-[#67C090]'>&lt;</span>Pass<span className='text-[#67C090]'>Fortress</span><span className='text-[#67C090]'>/&gt;</span></h1>
+
+          <div className=' border-2 border-[#67C090] px-5 py-1 rounded-[20px] font-bold text-white flex items-center justify-center hover:bg-green-500 cursor-pointer transition-all'>Login</div>
+        </nav>
+      </div>
+    </motion.div>
   )
 }
 
