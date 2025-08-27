18bc84cef10ded1891ce74093756e0a95d3ac29a
anonymous298 <tackletalha@gmail.com>
2025-08-27 12:50:31 +0500

    responsive design changes input size

--- components/PasswordManager.jsx
+++ components/PasswordManager.jsx
@@ -121,21 +121,21 @@ const PasswordManager = ({isloading}) => {
             </div>
 
             <form className='inputs flex flex-col gap-5' onSubmit={handleSubmit(onSubmit)}>
-              <input {...register('websiteURL', {required : true})} type="text" value={websiteURL} onChange={handleWebsiteURLInput} placeholder='Enter Website URL' className='placeholder:text-gray-600 w-[100%] border-2 border-[#67C090] rounded-[20px] p-1 outline-none px-3 max-[500px]:text-[13px] bg-[#DCD0A8] text-black active:bg-[#DCD0A8]' />
+              <input {...register('websiteURL', {required : true})} type="text" value={websiteURL} onChange={handleWebsiteURLInput} placeholder='Enter Website URL' className='placeholder:text-gray-600 w-[100%] border-2 border-[#67C090] rounded-[20px] p-1 outline-none px-3  bg-[#DCD0A8] text-black active:bg-[#DCD0A8] max-[780px]:py-2 max-[780px]:text-[17px]' />
 
               <div className='flex gap-3 max-[500px]:gap-1 max-[500px]:flex-col max-[500px]:gap-y-5'>
-                <input {...register('username', {required : true})} type="text" value={username} onChange={handleUsernameInput}  placeholder='Enter Username' className='placeholder:text-gray-600 w-[80%] max-[500px]:w-[100%] border-2 border-[#67C090] rounded-[20px] p-1 outline-none  px-3 max-[500px]:text-[13px] bg-[#DCD0A8] active:bg-[#DCD0A8] text-black' />
+                <input {...register('username', {required : true})} type="text" value={username} onChange={handleUsernameInput}  placeholder='Enter Username' className='placeholder:text-gray-600 w-[80%] max-[500px]:w-[100%] border-2 border-[#67C090] rounded-[20px] p-1 outline-none  px-3 max-[780px]:text-[17px] bg-[#DCD0A8] active:bg-[#DCD0A8] text-black max-[780px]:py-2' />
 
-                <div className=' border-2 border-[#67C090] rounded-[20px] p-1 flex justify-between px-3 bg-[#DCD0A8]'>
-                  <input {...register('password', {required : true})} type={showPassword ? 'text ' : 'password'} value={password} onChange={handlePasswordInput}  placeholder='Enter Password' className=' placeholder:text-gray-600 outline-none w-[80%] px-2 max-[500px]:px-0.5 max-[500px]:text-[13px] active:bg-[#DCD0A8] text-black'/>
+                <div className=' border-2 border-[#67C090] rounded-[20px] p-1 flex justify-between px-3 bg-[#DCD0A8] max-[780px]:items-center'>
+                  <input {...register('password', {required : true})} type={showPassword ? 'text ' : 'password'} value={password} onChange={handlePasswordInput}  placeholder='Enter Password' className=' placeholder:text-gray-600 outline-none w-[80%] px-2 max-[500px]:px-0.5 max-[780px]:text-[17px] active:bg-[#DCD0A8] text-black max-[780px]:py-2'/>
 
                   <div onClick={toggleShowPassword} className='cursor-pointer'>
-                    {showPassword ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 fill-[#004030]">
+                    {showPassword ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 fill-[#004030] max-[780px]:size-8">
                                       <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
                                       <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z" clipRule="evenodd" />
                                     </svg> :
                   
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 fill-[#004030]">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 fill-[#004030] max-[780px]:size-8">
                                       <path d="M3.53 2.47a.75.75 0 0 0-1.06 1.06l18 18a.75.75 0 1 0 1.06-1.06l-18-18ZM22.676 12.553a11.249 11.249 0 0 1-2.631 4.31l-3.099-3.099a5.25 5.25 0 0 0-6.71-6.71L7.759 4.577a11.217 11.217 0 0 1 4.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113Z" />
                                       <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0 1 15.75 12ZM12.53 15.713l-4.243-4.244a3.75 3.75 0 0 0 4.244 4.243Z" />
                                       <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 0 0-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 0 1 6.75 12Z" />
