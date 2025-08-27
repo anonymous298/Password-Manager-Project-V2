3ea451b63e516bec3d858cd2587c06091a60cb17
anonymous298 <tackletalha@gmail.com>
2025-08-27 11:23:35 +0500

    animation added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,15 +1,23 @@
 "use client"
 
-import React from 'react'
+import React from 'react';
+import * as motion from "motion/react-client";
 
 const Footer = () => {
   return (
-    <div className='bg-[#004030] p-1 w-[100%] flex justify-center items-center mt-5'>
-        <footer className=' text-center'>
-            <h1 className="logo text-white font-bold text-[18px] cursor-pointer"><span className='text-[#67C090]'>&lt;</span>Pass<span className='text-[#67C090]'>Fortress</span><span className='text-[#67C090]'>/&gt;</span></h1>
-            <p className='text-white text-[14px] font-semibold space-x-5'>Created With ❤️ By Talha</p>
-        </footer>
-    </div>
+    <motion.div
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.2, ease: "easeOut" }}
+    >
+
+          <div className='bg-[#004030] p-1 w-[100%] flex justify-center items-center mt-5'>
+              <footer className=' text-center'>
+                  <h1 className="logo text-white font-bold text-[18px] cursor-pointer"><span className='text-[#67C090]'>&lt;</span>Pass<span className='text-[#67C090]'>Fortress</span><span className='text-[#67C090]'>/&gt;</span></h1>
+                  <p className='text-white text-[14px] font-semibold space-x-5'>Created With ❤️ By Talha</p>
+              </footer>
+          </div>
+    </motion.div>
   )
 }
 
